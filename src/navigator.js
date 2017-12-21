525c4a1798e28cb7b26eda2e2e04912e71282d81
JJMoon <jm99tt@gmail.com>
2017-12-21 16:14:07 +0900

    Add side menu from example.

--- src/navigator.js
+++ src/navigator.js
@@ -7,6 +7,8 @@ import SecondScreen from './screens/SecondScreen';
 import TabNavMain from './screens/TabNavigator';
 import DrawerInit from './screens/DrawerInit';
 
+import Drawer from './screens/Drawer';
+
 const Navigator = StackNavigator({
   Main: {
     navigationOptions: {
@@ -28,6 +30,11 @@ const Navigator = StackNavigator({
     },
   },
   Drawer: {
+    name: 'Drawer Example',
+    description: 'Android-style drawer navigation',
+    screen: Drawer,
+  },
+  DrawerInit: {
     screen: DrawerInit,
     navigationOptions: {
       header: null,
