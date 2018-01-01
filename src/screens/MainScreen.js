7626d9a9e70e8514effe24ad5963e2d89f89c694
Jongwoo Moon <jm99tt@gmail.com>
2018-01-01 10:15:14 +0900

    Add component

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -4,11 +4,13 @@ import { inject, observer } from 'mobx-react/native';
 import PropTypes from 'prop-types';
 import Interactable from 'react-native-interactable';
 
+import SomeText from '../Compo/SomeText';
 
 /**
-* JSDOC : MainScreen
-*/
-@inject('store') @observer
+ * JSDOC : MainScreen
+ */
+@inject('store')
+@observer
 class MainScreen extends Component {
   constructor(props) {
     super(props);
@@ -16,18 +18,18 @@ class MainScreen extends Component {
     this.state = { counter: 0 };
   }
 
-  onDrawerSnap = (prop) => {
+  onDrawerSnap = prop => {
     console.log('prop', prop);
-  }
+  };
 
-  onDragEvent = (prop) => {
+  onDragEvent = prop => {
     // console.log('onDragEvent', prop);
-  }
+  };
 
   closeTheView = () => {
     console.log('close the view');
     this.intView.snapTo({ x: 0 });
-  }
+  };
 
   componentWillMount() {
     console.log('\n\n MainScreen :: component Will Mount  >>> \n\n');
@@ -45,7 +47,8 @@ class MainScreen extends Component {
     console.log(' \t\t MainScreen :: component Will Receive Props');
   }
 
-  shouldComponentUpdate() { // 판단 식.
+  shouldComponentUpdate() {
+    // 판단 식.
     console.log(' \t\t MainScreen :: shouldComponentUpdate');
     return true;
   }
@@ -61,60 +64,39 @@ class MainScreen extends Component {
   render() {
     console.log('\n MainScreen :: render  >>> \n');
     const { navigate } = this.props.navigation;
-    const wUnit = 100, height = 70;
+    const wUnit = 100,
+      height = 70;
 
     return (
       <View style={{ flex: 1, backgroundColor: '#BFE' }}>
+        <Button title="Show Navigate Object" onPress={() => console.log(' navigator ', this.props.navigation)} />
 
-        <Button
-          title="Show Navigate Object"
-          onPress={() => console.log(' navigator ', this.props.navigation)}
-        />
+        <Button title="Go to Second Screen" onPress={() => navigate('Second')} />
 
-        <Button
-          title="Go to Second Screen"
-          onPress={() => navigate('Second')}
-        />
+        <Button title="Screen without Navigation Bar" onPress={() => navigate('SecondWithoutNavigationBar')} />
 
-        <Button
-          title="Screen without Navigation Bar"
-          onPress={() => navigate('SecondWithoutNavigationBar')}
-        />
+        <Button title="Go to Tab View" onPress={() => navigate('TabView')} />
 
-        <Button
-          title="Go to Tab View"
-          onPress={() => navigate('TabView')}
-        />
+        <Button title="Add ValGuide" onPress={() => this.props.store.addGuide()} />
 
-        <Button
-          title="Add ValGuide"
-          onPress={() => this.props.store.addGuide()}
-        />
-        <Button
-          title="Go go Drawer Screen"
-          onPress={() => navigate('Drawer')}
-        />
+        <Button title="Go go Drawer Screen" onPress={() => navigate('Drawer')} />
 
         <Button
           title={'Change State : ' + this.state.counter}
           onPress={() => this.setState({ counter: this.state.counter + 1 })}
         />
 
-        <View style={{ width: '100%', height, backgroundColor: '#0000' }} >
-          <View style={{ position: 'absolute', flexDirection: 'row', width: '100%', height: 70 }} >
-            <TouchableOpacity style={{ width: wUnit, height, backgroundColor: '#DA8' }}
-              onPress={this.closeTheView}
-            />
+        <SomeText title={'Title'} content={'Content from MainScreen'} />
+
+        <View style={{ width: '100%', height, backgroundColor: '#0000' }}>
+          <View style={{ position: 'absolute', flexDirection: 'row', width: '100%', height: 70 }}>
+            <TouchableOpacity style={{ width: wUnit, height, backgroundColor: '#DA8' }} onPress={this.closeTheView} />
             <View style={{ flex: 1, height, backgroundColor: '#AAA' }} />
-            <TouchableOpacity style={{ width: wUnit, height, backgroundColor: '#5DD' }}
-              onPress={this.closeTheView}
-            />
-            <TouchableOpacity style={{ width: wUnit, height, backgroundColor: '#D5D' }}
-              onPress={this.closeTheView}
-            />
+            <TouchableOpacity style={{ width: wUnit, height, backgroundColor: '#5DD' }} onPress={this.closeTheView} />
+            <TouchableOpacity style={{ width: wUnit, height, backgroundColor: '#D5D' }} onPress={this.closeTheView} />
           </View>
           <Interactable.View
-            ref={r => this.intView = r}
+            ref={r => (this.intView = r)}
             horizontalOnly
             snapPoints={[{ x: 0 }, { x: wUnit }, { x: -wUnit }, { x: -2 * wUnit }]}
             onSnap={this.onDrawerSnap}
@@ -122,9 +104,7 @@ class MainScreen extends Component {
           >
             <View style={{ width: '100%', height, backgroundColor: '#99A' }} />
           </Interactable.View>
-
         </View>
-
       </View>
     );
   }
@@ -132,7 +112,7 @@ class MainScreen extends Component {
 
 MainScreen.wrappedComponent.propTypes = {
   navigation: PropTypes.shape().isRequired,
-  store: PropTypes.shape().isRequired,
+  store: PropTypes.shape().isRequired
 };
 
 export default MainScreen;
